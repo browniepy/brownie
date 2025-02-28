ccb339f50b6e294ac36cca31f45af63adb2e6668
browniepy <perrosxdxd50@gmail.com>
2025-02-28 10:37:26 -0600

    pre and post command cache changes

--- brownie/src/client.rs
+++ brownie/src/client.rs
@@ -14,13 +14,13 @@ pub async fn build() -> Result<Client, Error> {
     let mut commands = vec![
         commands::profile::balance(),
         commands::profile::stl(),
+        commands::profile::inventory(),
+        commands::profile::message(),
         commands::work::work(),
-        commands::dth::dth(),
-        commands::zeronim::nim(),
-        commands::audio::audio(),
-        commands::oldmaid::oldmaid(),
-        commands::airpoker::airpoker(),
-        commands::blackjack::blackjack(),
+        commands::contradiction::contradiction(),
+        commands::dices::dices(),
+        commands::system::shop(),
+        commands::system::top(),
     ];
 
     let translations = read_ftl()?;
@@ -32,6 +32,9 @@ pub async fn build() -> Result<Client, Error> {
             .time_to_live(Duration::from_secs(300))
             .build(),
         translations,
+        system: Cache::builder()
+            .time_to_live(Duration::from_secs(600))
+            .build(),
     };
 
     let options = poise::FrameworkOptions {
@@ -44,14 +47,19 @@ pub async fn build() -> Result<Client, Error> {
             Box::pin(async move {
                 let command = &ctx.command().qualified_name;
                 let author = &ctx.author().name;
-
                 tracing::info!("{} used {}", author, command);
+
                 crate::cache(ctx, ctx.author().id).await;
+                crate::cache_system(ctx).await;
+            })
+        },
+        post_command: |ctx| {
+            Box::pin(async move {
+                let data = ctx.data();
 
                 let member = crate::get_member(ctx, ctx.author().id).await.unwrap();
                 let mut write = member.write().await;
-
-                write.reload_deck();
+                write.add_points(15, &data.pool).await.unwrap();
             })
         },
         on_error: |error| Box::pin(on_error(error)),
@@ -60,6 +68,7 @@ pub async fn build() -> Result<Client, Error> {
                 .empty_users()
                 .replied_user(false),
         ),
+        manual_cooldowns: true,
         ..Default::default()
     };
 
@@ -82,12 +91,11 @@ async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
     match error {
         poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
         poise::FrameworkError::Command { error, ctx, .. } => {
-            println!("Error in command `{}`: {:?}", ctx.command().name, error,);
-            ctx.reply(format!("{:?}", error)).await.unwrap();
+            ctx.reply(error.to_string()).await.unwrap();
         }
         error => {
             if let Err(e) = poise::builtins::on_error(error).await {
-                println!("Error while handling error: {}", e)
+                tracing::error!("error while handling error: {}", e);
             }
         }
     }
